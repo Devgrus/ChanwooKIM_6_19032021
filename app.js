678942095eb3b5e03fea717d84643e284959fb42
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-03-21 22:17:34 +0100

    création des fichiers

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const app = require('./app');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://dbuser:user@cluster0.u1id9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
