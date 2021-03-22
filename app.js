5f33b1f7f043aa1d9227733a765d3d2a9751a0a2
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-03-22 09:37:44 +0100

    modification : sauce.js, user.js dans le répertoire <models>

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = require('./app');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://dbuser:user@cluster0.u1id9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -17,5 +20,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
