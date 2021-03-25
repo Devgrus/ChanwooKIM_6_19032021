75a7eaf455a32b4d4d9569256b2cd32898b67c6f
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-03-25 08:49:31 +0100

    MAJ de models

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    //id : {type: ObjectId, required: true},
     userId : {type: String, required: true},
     name : {type: String, required: true},
     manufacturer : {type: String, required: true},
@@ -11,8 +10,8 @@ const sauceSchema = mongoose.Schema({
     heat : {type: Number, required: true},
     likes : {type: Number, required: false},
     dislikes : {type: Number, required: false},
-    usersLiked : {type: Number, required: false},
-    usersDisliked : {type: Number, required: false}
+    usersLiked : {type: Array, required: false},
+    usersDisliked : {type: Array, required: false}
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
